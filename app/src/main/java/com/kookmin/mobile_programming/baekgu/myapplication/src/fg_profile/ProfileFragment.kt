6b53edb5df3160a4843a47a786eed9be3a0f78c6
jisu Seo <jseo9732@gmail.com>
2022-12-04 18:58:34 +0900

    로그아웃 기능 구현 (임시 디자인)

--- app/src/main/java/com/kookmin/mobile_programming/baekgu/myapplication/src/fg_profile/ProfileFragment.kt
+++ app/src/main/java/com/kookmin/mobile_programming/baekgu/myapplication/src/fg_profile/ProfileFragment.kt
@@ -3,9 +3,12 @@ package com.kookmin.mobile_programming.baekgu.myapplication.src.fg_profile
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.kookmin.mobile_programming.baekgu.myapplication.R
 import com.kookmin.mobile_programming.baekgu.myapplication.config.BaseFragment
 import com.kookmin.mobile_programming.baekgu.myapplication.databinding.FragmentProfileBinding
+import com.kookmin.mobile_programming.baekgu.myapplication.src.login.LoginActivity
 import com.kookmin.mobile_programming.baekgu.myapplication.src.profile_edit.ProfileEditActivity
 
 class ProfileFragment: BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::bind, R.layout.fragment_profile) {
@@ -19,7 +22,13 @@ class ProfileFragment: BaseFragment<FragmentProfileBinding>(FragmentProfileBindi
 
     private fun setListener(){
         binding.fgProfileTvProfileEdit.setOnClickListener {
-            var intent= Intent(requireContext(),ProfileEditActivity::class.java)
+            var intent= Intent(requireContext(), ProfileEditActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.fgProfileTvLogout.setOnClickListener {
+            Firebase.auth.signOut()
+            var intent= Intent(requireContext(), LoginActivity::class.java)
             startActivity(intent)
         }
     }
