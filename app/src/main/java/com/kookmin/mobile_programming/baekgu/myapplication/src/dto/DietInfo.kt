e4d9fffd189dd4a49d6771f183cbbd484c42a137
wnsdnjs70 <wnsdnjs700@naver.com>
2022-12-05 15:35:15 +0900

    Junwon / add calendar

---
+++ app/src/main/java/com/kookmin/mobile_programming/baekgu/myapplication/src/dto/DietInfo.kt
@@ -0,0 +1,18 @@
+package com.kookmin.mobile_programming.baekgu.myapplication.src.dto
+
+
+class DietInfo (){
+
+    var calendar = arrayOf(
+        Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}),
+        Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}),
+        Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}),
+        Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}),
+        Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}),
+        Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}),
+        Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}),
+        Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}),
+        Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}),
+        Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}),Array(6,{"0"}))
+
+}
