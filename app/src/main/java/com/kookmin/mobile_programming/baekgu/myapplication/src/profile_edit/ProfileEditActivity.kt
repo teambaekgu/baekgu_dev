d58feb39e7471c09819bf3536376a8c88bcf1c88
jisu Seo <jseo9732@gmail.com>
2022-12-05 13:44:11 +0900

    회원 정보 조회 기능 구현

--- app/src/main/java/com/kookmin/mobile_programming/baekgu/myapplication/src/profile_edit/ProfileEditActivity.kt
+++ app/src/main/java/com/kookmin/mobile_programming/baekgu/myapplication/src/profile_edit/ProfileEditActivity.kt
@@ -9,6 +9,21 @@ class ProfileEditActivity:BaseActivity<ActivityProfileEditBinding>(ActivityProfi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val sharedPreference = getSharedPreferences("userInfo", MODE_PRIVATE)
+        val emailValue = sharedPreference.getString("email", "email")
+        val pwValue = sharedPreference.getString("password", "password")
+        val nameValue = sharedPreference.getString("name", "name")
+        val birthValue = sharedPreference.getString("birth", "birth")
+        val phoneValue = sharedPreference.getString("phone", "phone")
+        val addressValue = sharedPreference.getString("address", "address")
+
+        binding.profileEditEditId.setText(emailValue)
+        binding.profileEditEditPw.setText(pwValue)
+        binding.profileEditEditName.setText(nameValue)
+        binding.profileEditEditBirthday.setText(birthValue)
+        binding.profileEditEditNumber.setText(phoneValue)
+        binding.profileEditEditTown.setText(addressValue)
+
         setListener()
     }
 
